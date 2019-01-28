45030975c2c0e50a6c8df0bd5216e0359c4b0458
linwenyao <linwenyao@flytexpress.com>
2019-01-28 14:33:37 +0800

    add artDialog

--- index.js
+++ index.js
@@ -14,40 +14,18 @@ $(document).ready(function() {
     }
   }());
 
-  // 默认设置
+  // dataTables默认设置
   $.extend($.fn.dataTable.defaults,
   {
-    language: {
-        "sProcessing": "处理中...",
-        "sLengthMenu": "显示 _MENU_ 项结果",
-        "sZeroRecords": "没有匹配结果",
-        "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
-        "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
-        "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
-        "sInfoPostFix": "",
-        "sSearch": "搜索:",
-        "sUrl": "",
-        "sEmptyTable": "表中数据为空",
-        "sLoadingRecords": "载入中...",
-        "sInfoThousands": ",",
-        "oPaginate": {
-            "sFirst": "首页",
-            "sPrevious": "上页",
-            "sNext": "下页",
-            "sLast": "末页"
-        },
-        "oAria": {
-            "sSortAscending": ": 以升序排列此列",
-            "sSortDescending": ": 以降序排列此列"
-        }
-      }
+    searching: false,// 禁用搜索
+    ordering: false,// 禁用排序
+    dom: "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-5 page-info'li><'col-sm-7'p>>"
   });
   
   //初始化dataTables
   $('#example').DataTable({
-    ordering:  false,// 禁用排序
     data: dataList,
-    scrollY: 400 // 表格里使用滚动
+    scrollY: 400, // 表格里使用滚动
   });
 
 });
